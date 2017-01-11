d7eb743f7603eddfaea021d125cfb58c5224f770
aris-kun <ariskyun@gmail.com>
2017-01-11 22:48:00 +0700

    New system

--- index.php
+++ index.php
@@ -97,7 +97,7 @@ switch (ENVIRONMENT)
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
  */
-	$system_path = 'System/CodeIgniter-3.1.2';
+	$system_path = 'System';
 
 /*
  *---------------------------------------------------------------
